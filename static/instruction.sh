107574119413912c7155b03daf13a0077039f1ff
Daniel Hansson <mailto@danielhansson.nu>
2023-03-20 17:55:02 +0100

    Update instruction.sha

--- static/instruction.sh
+++ static/instruction.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 
 # T&M Hansson IT AB © - 2023, https://www.hanssonit.se/
 
@@ -9,11 +8,11 @@ Color_Off='\e[0m'       # Text Reset
 clear
 cat << INST1
 +-----------------------------------------------------------------------+
-|      Welcome to the first setup of your own Jitsi Server! :)      |
+|      Welcome to the first setup of your own Jitsi Server! :)          |
 |                                                                       |
 INST1
 echo -e "|"  "${IGreen}To run the startup script type the sudoer password, then hit [ENTER].${Color_Off} |"
-echo -e "|"  "${IGreen}The default sudoer password is: ${BIGreen}jitsi${IGreen}${Color_Off}                             |"
+echo -e "|"  "${IGreen}The default sudoer password is: ${BIGreen}jitsi${IGreen}${Color_Off}                                 |"
 cat << INST2
 |                                                                       |
 | You can find the complete install instructions here:                  |
