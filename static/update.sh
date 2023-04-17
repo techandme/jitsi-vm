c30f691db16120c2174963eb86d6a08b627e15c4
Daniel Hansson <mailto@danielhansson.nu>
2023-04-17 12:52:16 +0200

    implement update script

---
+++ static/update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# T&M Hansson IT AB © - 2023, https://www.hanssonit.se/
+
+true
+SCRIPT_NAME="Update Server + Jitsi"
+# shellcheck source=lib.sh
+source <(curl -sL https://raw.githubusercontent.com/nextcloud/vm/master/lib.sh)
+
+# Check for errors + debug code and abort if something isn't right
+# 1 = ON
+# 0 = OFF
+DEBUG=0
+debug_mode
+
+# Must be root
+root_check
+
+mkdir -p "$SCRIPTS"
+
+# Delete, download, run
+run_script STATIC jitsi_update
+
+exit
